8da92b804a9fd8edad57adf4ce2b89cf8a6bd042
Lord Jerry <lovinjerry004@gmail.com>
2020-01-06 09:29:25 +0000

    read download directory and get al files in it

--- index.js
+++ index.js
@@ -5,18 +5,27 @@ const fs = require('fs');
 class FileManager {
 
   static home = process.env.HOME;
-  static download = `${this.home}/Downloads2`;
+  static downloads = `${this.home}/Downloads`;
   static pictures = `${this.home}/Pictures`;
   static vidoes = `${this.home}/Videos`;
   static music = `${this.home}/Music`;
 
+  static files = [];
+
   static async getDownloads() {
     try {
-      await fs.promises.opendir(this.download);
+      // check if the download directory is available
+      await fs.promises.opendir(this.downloads);
+
+      const temp = fs.readdirSync(this.downloads);
+      this.files.push(...temp);
+      console.log(this.files, 1);
     } catch (e) {
       if (e.code && e.code === 'ENOENT') {
-        console.error(e.message)
+        console.error(e.message);
+        return;
       }
+      console.log(err);
     }
   }
 
